558cd270198da6223ec3533734b58426c79ec507
haeunhye <ha9820@naver.com>
2018-04-14 17:03:48 +0900

    동전1

---
+++ Desktop/2293.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int d[10001];
+int a[101];
+int main(){
+    int n, k;
+	scanf("%d %d", &n, &k);
+	for(int i=1; i<=n; i++){
+		scanf("%d", &a[i]);
+	}
+	d[0] = 1;
+	for(int i=1; i<=n; i++){
+		for(int j=0; j<=k; j++){
+            if(j-a[i]>=0){
+			d[j] += d[j-a[i]];
+            }
+		}
+	}
+	printf("%d\n", d[k]);
+	return 0;
+}
